315042d54397030df26946e8a366e198db1ef554
Matheus <matheus.181137@gmail.com>
2018-05-31 15:17:19 -0400

    form added

--- v1/app.js
+++ v1/app.js
@@ -1,22 +1,36 @@
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+let campgrounds = [
+    {name: "Salmon Creek", image:"https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
+    
+    {name: "Grenade Hill", image:"https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
+
+    {name: "Mountain Goat's Rest", image:"https://images.pexels.com/photos/746758/pexels-photo-746758.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}
+]
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-    let campgrounds = [
-        {name: "Salmon Creek", image:"https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
-        
-        {name: "Grenade Hill", image:"https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
-
-        {name: "Mountain Goat's Rest", image:"https://images.pexels.com/photos/746758/pexels-photo-746758.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}
-    ]
-
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", (req, res) => {
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new.ejs");
 });
 
 app.listen(3000, () => {
