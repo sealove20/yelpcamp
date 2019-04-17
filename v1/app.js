112e459aa2923db437caeaab2ec3e42823306896
Matheus Henrique <matheus.181137@gmail.com>
2019-04-16 20:49:47 -0400

    bckp

--- v1/app.js
+++ v1/app.js
@@ -1,15 +1,46 @@
-let express = require("express");
-let app = express();
-let bodyParser = require("body-parser");
+let express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create(
+    {
+        name: "Salmon Creek",
+        image:"https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+    }, (err, campground) => {
+        if(err){
+            console.log(err);
+            console.log("deuruim")
+        } else {
+            console.log("NEWLY CREATED CAMPGROUND: ");
+            console.log(campground);
+        }
+    });
+
+
+
+
 let campgrounds = [
     {name: "Salmon Creek", image:"https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
-    
     {name: "Grenade Hill", image:"https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
-
+    {name: "Mountain Goat's Rest", image:"https://images.pexels.com/photos/746758/pexels-photo-746758.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
+    {name: "Salmon Creek", image:"https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
+    {name: "Grenade Hill", image:"https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
+    {name: "Mountain Goat's Rest", image:"https://images.pexels.com/photos/746758/pexels-photo-746758.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
+    {name: "Salmon Creek", image:"https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
+    {name: "Grenade Hill", image:"https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
     {name: "Mountain Goat's Rest", image:"https://images.pexels.com/photos/746758/pexels-photo-746758.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}
 ]
 
