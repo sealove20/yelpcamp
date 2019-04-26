c457172fa736ada2dd5d0a473c5c11224df6ae50
Matheus Henrique <matheus.181137@gmail.com>
2019-04-26 15:24:02 -0400

    v7 done v8

---
+++ v8/routes/campgrounds.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+const Campground = require("../models/campground");
+
+//Index - show all campgrounds
+router.get("/", (req, res) => {
+  // Get all campgrounds from db
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      consoe.log(err);
+    } else {
+      res.render("campgrounds/index", {
+        campgrounds: allCampgrounds
+      });
+    }
+  });
+  // res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+//create - add new campground to DB
+router.post("/", (req, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
+  let desc = req.body.description;
+  let newCampground = { name: name, image: image, description: desc };
+  // Create a new campground and save to DB
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect back to campgrounds page
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
+// new - show form to create new campgrund
+router.get("/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+//show - shows more info about one campground
+router.get("/:id", (req, res) => {
+  // find the campground with provided ID
+  Campground.findById(req.params.id)
+    .populate("comments")
+    .exec((err, foundCampground) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(foundCampground);
+        // render show template with that campground
+        res.render("campgrounds/show", { campground: foundCampground });
+      }
+    });
+});
+
+// middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+module.exports = router;
