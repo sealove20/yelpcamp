4334408edd7b5254d27bb93ad23a34bf17c3fa2b
Matheus Henrique <matheus.181137@gmail.com>
2019-05-01 14:50:46 -0400

    v10 done v11

---
+++ v11/models/campground.js
@@ -0,0 +1,31 @@
+let mongoose = require("mongoose");
+const Comment = require("./comment");
+
+let campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
+});
+
+campgroundSchema.pre("remove", async function() {
+  await Comment.remove({
+    _id: {
+      $in: this.comments
+    }
+  });
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
